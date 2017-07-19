b76df610a11b19a649edcd0c2309688627fe1ed5
CloudyZx <yexiamcl0udy@gmail.com>
2017-07-19 19:27:46 +0530

    Add files via upload

---
+++ cloudy.py
@@ -0,0 +1,88 @@
+#!/bin/bash
+echo $red 
+ echo ""----------------------------------------------------------------------------- ""
+echo " This Script is written by Cloudy2.0 YouTube Channel. Enjoy Hacker's"
+echo "---------------------------------------------------------------------------------"
+
+echo "007"
+echo "007"
+echo "007"
+echo "."
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    CLOUDY2.0      "               
+ echo   " FOR TECHNOLOGY &  GREY HACKING VIDEOS "   
+ 
+echo   "####################################"
+
+echo "007"
+echo "007"
+echo "007"
+
+echo "."
+
+apt update -y                      #update in progress
+apt install python -y           #installation......
+apt install python2 -y
+apt install ruby -y 
+apt install sqlmap -y 
+apt install git -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    CLOUDY2.0      "               
+ echo   " FOR TECHNOLOGY &  GREY HACKING VIDEOS " 
+ 
+echo   "####################################"
+
+apt install php -y 
+apt install perl -y
+apt install nmap -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    CLOUDY2.0      "               
+ echo   " FOR TECHNOLOGY &  GREY HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install bash -y
+apt install clang -y
+apt install jq -y
+apt install macchanger -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    CLOUDY2.0      "               
+ echo   " FOR TECHNOLOGY &  GREY HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install nano -y
+apt install curl -y
+apt install tar -y
+apt install zip -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    CLOUDY2.0      "               
+ echo   " FOR TECHNOLOGY &  GREY HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install unzip -y
+apt install tor -y 
+apt install sudo -y
+apt install wget -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    CLOUDY2.0      "               
+ echo   " FOR TECHNOLOGY &  GREY HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+apt install wcalc -y 
+apt install openssl
+apt install bmon -y
+
+echo  "#####################################"
+echo   "  SUBSCRIBE    CLOUDY2.0      "               
+ echo   " FOR TECHNOLOGY &  GREY HACKING VIDEOS "                                                                    
+echo   "####################################"
+
+echo " If You Connected To Internet Then  "
+echo " YOUR PACKAGES IS INSTALLED SUCCESSFULLY "
+
+exit
